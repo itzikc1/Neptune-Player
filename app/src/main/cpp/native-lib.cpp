bb386be89acfe7fe38ec2ba68856ff3b0b3c1edc
itzik <itzikc2.ic@gmail.com>
2020-04-06 19:11:36 +0300

    first commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_inc_neptune_1player_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
